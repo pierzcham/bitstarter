d031ada211940c5a2603377e6252cb8eaff32344
Clyde Smith <example@clyde.edu>
2013-07-17 21:34:34 +0000

    tresc index.html do wyswietlenia oraz bufor

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require("fs");
-
+buf = new Buffer(1024);
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html"));
+    response.send(fs.readFileSync("index.html")+buf.toString());
 });
 
 var port = process.env.PORT || 5000;
